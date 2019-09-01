e2c42274b861392e224730dfcaea8ba212ebbdcc
M.C. Tan <mariechantaltan@gmail.com>
2019-09-01 01:23:20 -0400

    Update binary_tree_2.cpp

--- binary_tree_2.cpp
+++ binary_tree_2.cpp
@@ -464,7 +464,7 @@ void mirror(Tree *r)
   r->left = tmp;
 }
 
-Tree * add_to_BST(char arr[], int start, int end)
+Tree *add_to_BST(char arr[], int start, int end)
 {
   // create a new tree from a sorted array
 
@@ -476,7 +476,196 @@ Tree * add_to_BST(char arr[], int start, int end)
   int mid = (start + end)/2;
 
   Tree *r = new Tree;
+
   r->data = arr[mid];
   r->left = add_to_BST(arr, start, mid - 1);
-  r->right = add_to_
+  r->right = add_to_BST(arr, mid + 1, end);
+
+  return r;
+}
+
+Tree *create_minimal_BST(char arr[], int size)
+{
+  return add_to_BST(arr, 0, size - 1);
+}
+
+void breadth_first_traversal(Tree *root)
+{
+  if(root == NULL)
+  {
+    return;
+  }
+
+  deque <Tree *> que;
+  queue.push_back(root);
+
+  while(!queue.empty())
+  {
+    Tree *p = queue.front();
+
+    cout << p->data << " ";
+    
+    queue.pop_front();
+
+    if(p->left != NULL)
+    {
+      queue.push_back(p->left);
+    }
+
+    if(p->right != NULL)
+    {
+      queue.push_back(p->right);
+    }
+  }
+
+  cout << endl;
+}
+
+int get_level(struct Tree *node, int elm, int level)
+{
+  if(node == NULL)
+  {
+    return 0;
+  }
+
+  if(elm == node->data)
+    return level;
+  }
+
+  else if(elm < node->data)
+  {
+    return get_level(node->left, elm, level + 1);
+  }
+
+  else
+  {
+    return get_level(node->right, elm, level + 1);
+  }
+}
+
+void nth_max(struct Tree* t)
+{
+  static int n_max = 5;
+  static int num = 0;
+
+  if(t == NULL)
+  {
+    return;
+  }
+
+  nth_max(t->right);
+
+  ++num;
+
+  if(num == n_max)
+  {
+    cout << n_max << "-th maximum data is " << t->data << endl;
+    nth_max(t->left);
+  }
+}
+
+void tree_to_array(struct Tree *node, int a[])
+{
+  static int pos = 0;
+
+  if(node)
+  {
+    tree_to_array(node->left, a);
+    a[++pos] = node->data;
+    tree_to_array(node->right, a);
+  }
+}
+
+void level_even_odd(struct Tree *node)
+{
+  vector<char> even_vec, odd_vec;
+  if(node == NULL)
+  {
+    return;
+  }
+  deque<struct Tree*> que;
+  que.push_back(node);
+
+  while(!que.empty())
+  {
+    struct Tree *p = que.front();
+    int level = get_level(node, p->data, 0);
+    if(level % 2 == 0)
+    {
+      even_vec.push_back(p->data);
+    }
+    else
+    {
+      odd_vec.push_back(p->data);
+    }
+    que.pop_front();
+    if(p->left)
+    {
+      que.push_back(p->left);
+    }
+    if(p->right)
+    {
+      que.push_back(p->right);
+    }
+  }
+
+  cout << "even level elements: ";
+  for(int i = 0; i < even_vec.size(); ++i)
+  {
+    cout << even_vec[i] << " ";
+  }
+
+  cout << end << "odd level elements: ";
+
+  for(int i = 0; i < odd_vec.size(); ++i)
+  {
+    cout << odd_vec[i] << " ";
+  }
+
+  cout << endl;
+}
+
+int main(int argc, char **argv)
+{
+  char ch, ch1, ch2;
+  Tree *found;
+  Tree *succ;
+  Tree *pred;
+  Tree *ancestor;
+  char charArr[9] = {'A','B','C','D','E','F','G','H','I'};
+
+  Tree *root = new_tree_node('F');
+	insert_tree_node(root,'B');
+	insert_tree_node(root,'A');
+	insert_tree_node(root,'D');
+	insert_tree_node(root,'C');  
+	insert_tree_node(root,'E');
+	insert_tree_node(root,'G');
+	insert_tree_node(root,'I');
+	insert_tree_node(root,'H');
+
+  is_BST(root);
+
+  cout << "size = " << tree_size(root) << endl;
+  cout << "max depth = " << max_depth(root) << endl;
+  cout << "min depth = " << min_depth(root) << endl;
+  if(is_balanced(root))
+	{
+    cout << "Balanced" << endl;
+  }
+	else
+	{
+    cout << "Unbalanced" << endl;
+  }
+  if(root) 
+	{
+    cout << "Min value = " << min_tree(root)->data << endl;
+  }
+  if(root) 
+	{
+    cout << "Max value = " << max_tree(root)->data << endl;
+  }
+  cout << "Node B is at level: " << get_level(root, 'B', 0) << endl;
+	cout << "Node H is at level: " << get_level(root, 'H', 0) << endl;
+	cout << "Node F is at level: " << get_level(root, 'F', 0) << endl;
 }
